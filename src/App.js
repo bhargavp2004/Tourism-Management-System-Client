0083f18a86f0f7a7b303a3d31358efa803160f21
bhargavp2004 <105142165+bhargavp2004@users.noreply.github.com>
2023-08-17 09:11:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <>
+    <Signup></Signup>
+    </>
+  );
+}
+
+export default App;
