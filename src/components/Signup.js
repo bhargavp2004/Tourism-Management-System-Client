98d5f3aa47d3ed30f0a0424ef590b1f580104b43
bhargavp2004 <105142165+bhargavp2004@users.noreply.github.com>
2023-08-31 09:36:28 +0530

    commit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import tourismImage from '../images/Tourism-Jobs.jpg';
 
 export default function Signup() {
   const navigate = useNavigate();
@@ -136,8 +136,7 @@ export default function Signup() {
                     </div>
                     <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                        className="img-fluid" alt="" />
+                      <img src={tourismImage} className="img-fluid" alt="" />
 
                     </div>
                   </div>
