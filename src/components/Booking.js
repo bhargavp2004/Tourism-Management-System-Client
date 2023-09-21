9f744bd54127dac899bea6ad06883eda055141ff
rahul-nagvadia <nagvadiarahul02@gmail.com>
2023-09-21 17:01:35 +0530

    commit

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import { useParams } from "react-router-dom";
 function Booking() {
   const { id } = useParams();
   const [packages, setPackages] = useState({});
