d633646b72d580225f8b4abf80e9f8ba021b3d0d
rahul-nagvadia <nagvadiarahul02@gmail.com>
2023-09-21 09:50:06 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PackageCard from "./packageCard";
-
+import '../styles/homeuser.css'
 function Home() {
   const navigate = useNavigate();
   const [packages, setPackages] = useState([]);
