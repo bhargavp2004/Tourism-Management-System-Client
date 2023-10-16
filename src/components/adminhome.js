58b0e3ab27e055a4a0e3ca2ee5da8c5c153bca74
bhargavp2004 <105142165+bhargavp2004@users.noreply.github.com>
2023-10-16 23:09:48 +0530

    commit

--- src/components/adminhome.js
+++ src/components/adminhome.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import MyCard from './Card';
 import img1 from '../images/package.jpg';
+
 function App() {
-  const rowStyle = {
-    marginBottom: '40px', // Adjust the value to control the spacing
-  };
   return (
-    <div className="container">
-      <div className="row" style={rowStyle}>
-        <div className="col-md-4">
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-md-3 mb-4">
           <MyCard
             img={img1}
             title="Package"
@@ -17,40 +15,34 @@ function App() {
             update="/updatepack"
           />
         </div>
-        <div className="col-md-4">
+        <div className="col-md-3 mb-4">
           <MyCard
             img={img1}
             title="Guide"
             desc="Add Guide"
             add="/addguide"
             update="/updateguide"
-           
           />
         </div>
-        <div className="col-md-4">
+        <div className="col-md-3 mb-4">
           <MyCard
             img={img1}
             title="Place"
             desc="Add Some Places"
             add="/addplace"
             update="/updateplace"
-           
           />
         </div>
-        <div className="col-md-4">
+        <div className="col-md-3 mb-4">
           <MyCard
             img={img1}
             title="Add Dates"
             desc="Add Dates to Package"
             add="/adddate"
             update="/updatedate"
-           
           />
         </div>
       </div>
-      
-
-      
     </div>
   );
 }
